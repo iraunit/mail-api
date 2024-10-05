0e8cc370f578e7f11f767b2d20baa950926e3fab
iraunit <ramgopal10258@gmail.com>
2024-10-06 03:28:41 +0530

    testing done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import os
 import smtplib
 import ssl
+from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -15,7 +16,7 @@ API_KEY = os.environ.get("API_KEY")
 GMAIL_USERNAME = os.environ.get("GMAIL_USERNAME")
 GMAIL_PASSWORD = os.environ.get("GMAIL_PASSWORD")
 
-GMAIL_PORT = 465
+GMAIL_PORT = 587
 GMAIL_SMTP_SERVER = "smtp.gmail.com"
 
 AHA_USERNAME = os.environ.get("AHA_USERNAME")
@@ -24,28 +25,31 @@ AHA_PORT = 587
 AHA_SMTP_SERVER = 'send.ahasend.com'
 
 
-def send_plain_email_through(smtp_server, port, username, password, from_email, to_email, subject, body):
+def send_plain_email(smtp_server, port, username, password, from_email, to_email, subject, body, sender_name):
     try:
-        message = f"""\
-        Subject: ${subject}
-    
-        ${body}"""
+        msg = EmailMessage()
+        msg.set_content(body)
+        msg['Subject'] = subject
+        msg['From'] = f"{sender_name} <{from_email}>"
+        msg['To'] = to_email
 
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
             server.login(username, password)
-            server.sendmail(from_email, to_email, message)
+            server.send_message(msg)
         return True
     except Exception as e:
         logger.error(e)
         return False
 
 
-def send_fancy_email(smtp_server, port, username, password, from_email, to_email, subject, plain_body, fancy_body):
+def send_fancy_email(smtp_server, port, username, password, from_email, to_email, subject, plain_body, fancy_body,
+                     sender_name):
     try:
         message = MIMEMultipart("alternative")
         message["Subject"] = subject
-        message["From"] = from_email
+        message['From'] = f"{sender_name} <{from_email}>"
         message["To"] = to_email
 
         part1 = MIMEText(plain_body, "plain")
@@ -55,7 +59,8 @@ def send_fancy_email(smtp_server, port, username, password, from_email, to_email
         message.attach(part2)
 
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
             server.login(username, password)
             server.sendmail(
                 from_email, to_email, message.as_string()
@@ -71,51 +76,57 @@ def hello_world():
     return 'Server is Running'
 
 
-@app.route('/sendMail')
-def send_mail(method='POST'):
-    if method != 'POST':
-        return 'Not Allowed'
-    else:
-        logger.debug(request.json, request.method, request.headers, request.remote_addr, request.url)
-        from_email = request.json.get('from')
-        to_email = request.json.get('to')
-        subject = request.json.get('subject')
-        plain_body = request.json.get('plain_body')
-        fancy_body = request.json.get('fancy_body')
-        is_html = request.json.get('html')
-        api_key = request.json.get('api_key')
-        agent = request.json.get('agent')
-        if api_key != API_KEY:
-            return 'Invalid API Key', 403
+@app.route('/send', methods=['POST'])
+def send_mail():
+    logger.debug(request.json, request.method, request.headers, request.remote_addr, request.url)
+    from_email = request.json.get('from')
+    to_email = request.json.get('to')
+    subject = request.json.get('subject')
+    plain_body = request.json.get('plain_body')
+    fancy_body = request.json.get('fancy_body')
+    is_html = request.json.get('html')
+    api_key = request.json.get('api_key')
+    agent = request.json.get('agent')
+    sender_name = request.json.get('sender_name')
 
-        smtp_server = GMAIL_SMTP_SERVER
-        port = GMAIL_PORT
-        username = GMAIL_USERNAME
-        password = GMAIL_PASSWORD
+    smtp_server = GMAIL_SMTP_SERVER
+    port = GMAIL_PORT
+    username = GMAIL_USERNAME
+    password = GMAIL_PASSWORD
 
-        if agent == 'ahasend':
-            smtp_server = AHA_SMTP_SERVER
-            port = AHA_PORT
-            username = AHA_USERNAME
-            password = AHA_PASSWORD
+    if agent == 'ahasend':
+        smtp_server = AHA_SMTP_SERVER
+        port = AHA_PORT
+        username = AHA_USERNAME
+        password = AHA_PASSWORD
 
-        if from_email is None or to_email is None or subject is None or (plain_body is None and fancy_body is None):
+    if agent == 'other' or api_key != API_KEY:
+        smtp_server = request.json.get('smtp_server')
+        port = request.json.get('port')
+        username = request.json.get('username')
+        password = request.json.get('password')
+        if username is None or password is None or smtp_server is None or port is None or username == '' or password == '' or smtp_server == '' or port == '':
             return 'Missing Parameters', 400
-        else:
-            if is_html:
-                res = send_fancy_email(smtp_server, port, username, password, from_email, to_email, subject, plain_body,
-                                       fancy_body)
-                if res:
-                    return 'Success', 200
-                else:
-                    return 'Failed', 500
+
+    if not isinstance(port, int):
+        port = int(port)
+
+    if from_email is None or to_email is None or subject is None or sender_name is None or (
+            plain_body is None and fancy_body is None):
+        return f'Missing Parameters is from_email {from_email} to_email {to_email} subject {subject} sender_name {sender_name}', 400
+    else:
+        if is_html:
+            res = send_fancy_email(smtp_server, port, username, password, from_email, to_email, subject, plain_body,fancy_body,sender_name)
+            if res:
+                return 'Success', 200
             else:
-                res = send_plain_email_through(smtp_server, port, username, password, from_email, to_email, subject,
-                                               plain_body)
-                if res:
-                    return 'Success', 200
-                else:
-                    return 'Failed', 500
+                return 'Failed', 500
+        else:
+            res = send_plain_email(smtp_server, port, username, password, from_email, to_email, subject,plain_body,sender_name)
+            if res:
+                return 'Success', 200
+            else:
+                return 'Failed', 500
 
 
 if __name__ == '__main__':
